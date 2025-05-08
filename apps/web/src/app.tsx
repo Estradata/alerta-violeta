85aaa9da7a4705e6a58cb5c1174e850e19cb6623
Christian Velez <christian.gerardo_@outlook.es>
2025-05-08 16:32:03 -0600

    web: add shadcn

--- apps/web/src/app.tsx
+++ apps/web/src/app.tsx
@@ -1,36 +1,42 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import "./App.css";
-import { type PublicUser, publicUserSchema } from '@packages/validation/user-schema';
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import {
+  type PublicUser,
+  publicUserSchema,
+} from '@packages/validation/user-schema'
+import { Button } from '@/components/ui/button'
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [count, setCount] = useState(0)
   const data: PublicUser = {
-    id: "12",
-    name: "Daniel Martinez",
-    email: "hello@gmail.com",
-  };
+    id: '12',
+    name: 'Daniel Martinez',
+    email: 'hello@gmail.com',
+  }
 
   return (
     <>
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
+        <a href='https://vite.dev' target='_blank'>
+          <img src={viteLogo} className='logo' alt='Vite logo' />
         </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
+        <a href='https://react.dev' target='_blank'>
+          <img src={reactLogo} className='logo react' alt='React logo' />
         </a>
       </div>
       <h1>Vite + React</h1>
 
-      <p className="text-start">
+      <p className='text-start'>
         Is Valid?: <pre>{JSON.stringify(data, null, 4)}</pre>
       </p>
 
-      <p>{publicUserSchema.safeParse(data).success ? "Sí" : "No"}</p>
+      <Button>ok</Button>
 
-      <div className="card">
+      <p>{publicUserSchema.safeParse(data).success ? 'Sí' : 'No'}</p>
+
+      <div className='card'>
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
@@ -38,11 +44,11 @@ function App() {
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
       </div>
-      <p className="read-the-docs">
+      <p className='read-the-docs'>
         Click on the Vite and React logos to learn more
       </p>
     </>
-  );
+  )
 }
 
-export default App;
+export default App
--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -1,11 +1,15 @@
 // vite.config.js
 import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
 import path from 'path'
 
 export default defineConfig({
+   plugins: [react(), tailwindcss()],
    resolve: {
       alias: {
          '@packages': path.resolve(__dirname, '../../packages'),
+         '@': path.resolve(__dirname, './src'),
       },
    },
 })
