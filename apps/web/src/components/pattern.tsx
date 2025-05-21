46738ba6e0fae98d94b09624bdc5890223299888
Christian Velez <christian.gerardo_@outlook.es>
2025-05-21 15:50:21 -0600

    landing

--- apps/web/src/components/pattern.tsx
+++ apps/web/src/components/pattern.tsx
@@ -4,7 +4,7 @@ export default function Pattern(props: { className?: string }) {
       className={props.className}
       width='100%'
       height='100%'
-      viewBox='0 0 1512 982'
+      // viewBox='0 0 1512 982'
       fill='none'
       xmlns='http://www.w3.org/2000/svg'
     >
--- apps/web/src/routes/index.tsx
+++ apps/web/src/routes/index.tsx
@@ -2,6 +2,7 @@ import Pattern from '@/components/pattern'
 import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar'
 import { Button } from '@/components/ui/button'
 import { siteConfig } from '@/config'
+import { cn } from '@/lib/utils'
 import { createFileRoute, Link } from '@tanstack/react-router'
 import { MapPinnedIcon, ShieldAlertIcon } from 'lucide-react'
 
@@ -36,7 +37,7 @@ function HomeComponent() {
   return (
     <div className='w-full flex flex-col min-h-screen overflow-hidden'>
       <div className='bg-[#f6f6f6] w-full flex-1 grid grid-cols-12 pl-10 relative'>
-        <Pattern className='absolute -translate-x-1/6 [mask-image:linear-gradient(to_right,black,transparent)] top-0' />
+        <Pattern className='absolute md:-translate-x-1/6    [mask-image:linear-gradient(to_top,black,transparent)]  md:[mask-image:linear-gradient(to_right,black,transparent)] -bottom-60 md:top-0' />
 
         <div className='w-full px-10 py-4 flex justify-end col-span-12 absolute top-0 left-0'>
           <Button
@@ -92,38 +93,75 @@ function HomeComponent() {
 
         <div className='w-full h-full flex justify-center items-end col-span-12 md:col-span-7'>
           <div className='relative'>
-            <div className='absolute right-0 top-20 translate-x-1/2 rounded-full bg-white shadow-2xl flex items-center gap-3 p-2 pr-5 capitalize fade-in-once opacity-0'>
-              <div className='h-10 flex justify-center items-center aspect-square rounded-full bg-violet-600 text-white'>
-                <MapPinnedIcon />
-              </div>
-              Gestión de puntos seguros
-              <div className='absolute -right-2 -top-2 -translate-y-1/2 translate-x-1/2 rotate-180 opacity-0 an-opacity delay-2'>
-                <img src='/figure-2.svg' />
-              </div>
-            </div>
+            <FloatingBadge
+              position='top-right'
+              icon={<MapPinnedIcon className='size-4 md:size-6'/>}
+              text='Gestión de puntos seguros'
+            />
 
-            <div className='absolute left-0 bottom-40 -translate-x-1/2 rounded-full bg-white shadow-2xl flex items-center gap-3 p-2 pr-5 capitalize fade-in-once opacity-0 delay-1'>
-              <div className='h-10 flex justify-center items-center aspect-square rounded-full bg-violet-600 text-white'>
-                <ShieldAlertIcon />
-              </div>
-              Alertas en tiempo real
-              {/* 24x7 Customer Support */}
-              <div className='absolute -left-2 -bottom-2 translate-y-1/2 -translate-x-1/2 opacity-0 an-opacity delay-2'>
-                <img src='/figure-2.svg' />
-              </div>
-            </div>
+            <FloatingBadge
+              position='bottom-left'
+              icon={<ShieldAlertIcon className='size-4 md:size-6'/>}
+              text='Alertas en tiempo real'
+              delayClass='delay-1'
+            />
 
             <div className='absolute left-0 top-0 -translate-x-1/2 -translate-y-1/2'>
               <img src='/figure.svg' />
             </div>
 
             <img
-              src='/iphone.png'
+              src='/shot.png'
               alt='Mockup'
-              className='w-[300px] md:w-[400px] h-auto z-50'
+              className=' w-[300px] md:w-[400px] h-auto z-50'
             />
           </div>
         </div>
+      </div>
+    </div>
+  )
+}
+
+type FloatingBadgeProps = {
+  position: 'top-right' | 'bottom-left'
+  icon: React.ReactNode
+  text: string
+  delayClass?: string
+}
+function FloatingBadge({
+  position,
+  icon,
+  text,
+  delayClass = '',
+}: FloatingBadgeProps) {
+  const isTopRight = position === 'top-right'
+
+  return (
+    <div
+      className={cn(
+        `absolute ${
+          isTopRight
+            ? 'right-0 md:right-0 top-20 translate-x-2/12 md:translate-x-1/2'
+            : 'left-0 bottom-20 md:bottom-40  -translate-x-5/12 md:-translate-x-1/2'
+        } rounded-full bg-white shadow-2xl flex items-center gap-3 p-2 pr-5 capitalize fade-in-once opacity-0 ${delayClass}`,
+        'text-xs md:text-base',
+        'pointer-events-none'
+      )}
+    >
+      <div className='h-8 md:h-10 flex justify-center items-center aspect-square rounded-full bg-violet-600 text-white'>
+        {icon}
+      </div>
+
+      {text}
+
+      <div
+        className={`absolute ${
+          isTopRight
+            ? '-right-2 -top-2 -translate-y-1/2 translate-x-1/2 rotate-180'
+            : '-left-2 -bottom-2 translate-y-1/2 -translate-x-1/2'
+        } opacity-0 an-opacity delay-2`}
+      >
+        <img src='/figure-2.svg' />
       </div>
     </div>
   )
