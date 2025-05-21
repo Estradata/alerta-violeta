c378eff7ce1b7911004aefc0ff04e84dd84dd39b
Christian Velez <christian.gerardo_@outlook.es>
2025-05-21 11:38:11 -0600

    log env variables in web

--- apps/web/src/config/index.ts
+++ apps/web/src/config/index.ts
@@ -7,3 +7,9 @@ export const storagePrefix = `alerta_violeta`
 
 export const API_URL = import.meta.env.VITE_API_URL as string
 export const GOOGLE_MAPS_API_KEY = import.meta.env.VITE_GOOGLE_MAPS_API_KEY as string
+
+console.log('ENV:')
+console.log({
+  API_URL,
+  GOOGLE_MAPS_API_KEY
+})
